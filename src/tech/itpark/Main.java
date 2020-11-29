4102a5af0f68f0fcf15c5e89f3a35ec4175709d8
Airat-vagapov <airlake@yandex.ru>
2020-11-29 16:38:12 +0300

    Manager upd

--- src/tech/itpark/Main.java
+++ src/tech/itpark/Main.java
@@ -1,4 +1,35 @@
 package tech.itpark;
 
+import tech.itpark.manager.BookManager;
+import tech.itpark.model.Book;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class Main {
+    public static void main(String[] args) {
+        BookManager bookManager = new BookManager();
+        Book one = bookManager.save(new Book(
+            0,
+            "War and Piece",
+            "Lev Tolstoy"
+        ));
+
+        Book two = bookManager.save(new Book(
+                0,
+                "Anna Karenina",
+                "Tolstoy"
+        ));
+
+        Book three = bookManager.save(new Book(
+                0,
+                "The Brothers Karamazov",
+                "Fyodor Dostoyevsky"
+        ));
+        try {
+            System.out.println(bookManager.export());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/tech/itpark/manager/BookManager.java
+++ src/tech/itpark/manager/BookManager.java
@@ -3,18 +3,19 @@ package tech.itpark.manager;
 import tech.itpark.model.Book;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class BookManager {
-   private List<Book> items = new ArrayList<>();
+
+    private List<Book> items = new ArrayList<>();
    private int nextId = 1;
 
-   private Book save(Book item) {
+   public Book save(Book item) {
        if (item.getId() == 0) {
            item.setId(nextId);
            nextId++;
@@ -32,13 +33,16 @@ public class BookManager {
    }
 
 
-    private void export() throws IOException {
-        Path filePath = Paths.get("books.txt");
-        String csq = "";
-        // Как получить CSQ для всех объектов?
-        Files.writeString(filePath, "");
-        return;
+    public File export() throws IOException {
+        File file = new File("Books.txt");
+        file.createNewFile();
+        FileWriter fileWriter = new FileWriter(file);
 
+        for (Book item : items) {
+            fileWriter.write(item.getId() + ";" + item.getAuthor() + ";" + item.getName() + "\n");
+        }
+        fileWriter.close();
+        return file;
     }
 
-}
+    }
